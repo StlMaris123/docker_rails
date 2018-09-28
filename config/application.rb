48f89e29f2f35d8b109a20ce2d9990743d577883
StlMaris123 <stellamarisnjage@gmail.com>
2018-09-28 10:51:45 +0300

    Set up custom logger which logs to STDOUT

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Drkiq
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.log_level = :debug
+    config.log_tags = [:subdomain, :uuid]
+    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         {namespace: 'drkiq::cache'}
+
+   config.active_job.queue_adapter = :sidekiq
   end
 end
